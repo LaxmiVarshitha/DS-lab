b57b0615bb16f83724d1b7d830f75b2381036961
LaxmiVarshitha <120377480+LaxmiVarshitha@users.noreply.github.com>
2023-10-03 11:18:25 +0530

    Add files via upload

---
+++ link_list.c
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct node
+{
+int data;
+struct node *next;
+};
+struct node *head, *tail =NULL;
+
+void insert (int data)
+{
+struct node *newnode = (struct node*) malloc(sizeof (newnode));
+newnode -> data = data;
+newnode -> next = NULL;
+
+if (head == NULL)
+{
+head = newnode;
+tail = newnode;
+}
+else
+{
+tail -> next = newnode;
+tail = tail -> next;
+}
+
+return ;
+}
+
+
+void display()
+{
+struct node *current = head;
+
+if (head == NULL)
+{
+printf("empty list");
+return ;
+}
+
+
+printf("nodes of single linked list \n");
+
+while(current !=NULL)
+{
+printf("%d\n", current -> data);
+current= current -> next;
+}
+printf("\n");
+}
+
+
+int main()
+{
+insert (10);
+insert(79);
+insert (67);
+insert(33);
+display();
+return 0;
+}
+
+
+
+
+
+
+
+
+
